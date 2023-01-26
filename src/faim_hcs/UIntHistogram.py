c381a4eb191467e639eb9b60670d783eca8d5140
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2023-01-26 09:18:29 +0100

    Add UIntHistogram.

---
+++ src/faim_hcs/UIntHistogram.py
@@ -0,0 +1,240 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+class UIntHistogram:
+    def __init__(self, data=None):
+        """
+        An unsigned integer histogram class that can be updated with new data.
+
+        :param data: array[uint]
+            Optional data of which the initial histogram is built.
+        """
+        if data is not None:
+            assert data.min() >= 0, "Negative data is not supported."
+            self.offset, self.bins, self.frequencies = self._get_hist(data)
+        else:
+            self.offset, self.bins, self.frequencies = None, None, None
+
+    @staticmethod
+    def _add(list_a, list_b):
+        """
+        Add two lists element-wise.
+        :param list_a: list(numeric)
+        :param list_b: list(numeric)
+        :return: list(numeric)
+        """
+        return list(map(lambda e: e[0] + e[1], zip(list_a, list_b)))
+
+    @staticmethod
+    def _get_hist(data):
+        """
+        Compute histogram with integer bins.
+        :param data: array[numeric]
+        :return: offset, bins, frequencies
+        """
+        offset = int(data.min())
+        bins = int(data.max()) + 2 - offset
+        freq = list(np.histogram(data, np.arange(offset, offset + bins))[0])
+        return offset, bins, freq
+
+    def _aggregate_histograms(self, offset_data, bins, freq):
+        """
+        Integrate new frequencies into existing histogram.
+
+        :param offset_data:
+        :param bins:
+        :param freq:
+        :return:
+        """
+        assert isinstance(freq, list), "freq must be of type List."
+        assert isinstance(self.frequencies, list), "frequencies must be of type List."
+        lower_shift = offset_data - self.offset
+        upper_shift = self.offset + self.bins - (offset_data + bins)
+
+        if lower_shift == 0 and upper_shift == 0:
+            # Old and new frequencies cover the same range:
+            # [old frequencies]
+            # [new frequencies]
+            self.frequencies = self._add(self.frequencies, freq)
+        elif (
+            lower_shift < 0
+            and (offset_data + bins - 1 >= self.offset)
+            and (offset_data + bins - 1 <= self.offset + self.bins - 1)
+        ):
+            # New frequencies have additional lower ones.
+            #     [old frequencies]
+            # [new frequencies]
+            frequencies_to = offset_data + bins - self.offset - 1
+            freq_from = self.offset - offset_data
+            self.frequencies[:frequencies_to] = self._add(
+                self.frequencies[:frequencies_to],
+                freq[freq_from:],
+            )
+            self.frequencies = freq[:freq_from] + self.frequencies
+            self.offset = offset_data
+            self.bins = len(self.frequencies) + 1
+        elif lower_shift < 0 and (offset_data + bins - 1 < self.offset):
+            # New frequencies only have additional lower ones.
+            #                       [old frequencies]
+            # [new frequencies]
+            gap_freq = [
+                0,
+            ] * (self.offset - offset_data - bins + 1)
+            self.frequencies = freq + gap_freq + self.frequencies
+            self.offset = offset_data
+            self.bins = len(self.frequencies) + 1
+        elif (
+            offset_data >= self.offset
+            and offset_data <= (self.offset + self.bins - 2)
+            and offset_data + bins - 2 > (self.offset + self.bins - 2)
+        ):
+            # New frequencies have additional upper ones.
+            #     [old frequencies]
+            #            [new frequencies]
+            from_frequencies = self.offset + self.bins - offset_data - 1
+            to_freq = self.offset + self.bins - offset_data
+            self.frequencies[-from_frequencies:] = self._add(
+                self.frequencies[-from_frequencies:],
+                freq[:to_freq],
+            )
+            self.frequencies = self.frequencies + freq[from_frequencies:]
+            self.bins = len(self.frequencies) + 1
+        elif (offset_data) > (self.offset + self.bins - 2):
+            # New frequencies have only additional upper ones.
+            #     [old frequencies]
+            #                           [new frequencies]
+            gap_freq = [
+                0,
+            ] * (offset_data - self.offset - self.bins + 1)
+            self.frequencies = self.frequencies + gap_freq + freq
+            self.bins = len(self.frequencies) + 1
+        elif lower_shift >= 0 and upper_shift >= 0:
+            # New frequencies are completely covered.
+            # [          old frequencies          ]
+            #           [new frequencies]
+            if upper_shift == 0:
+                self.frequencies[lower_shift:] = self._add(
+                    self.frequencies[lower_shift:], freq
+                )
+            else:
+                self.frequencies[lower_shift:-upper_shift] = self._add(
+                    self.frequencies[lower_shift:-upper_shift], freq
+                )
+        else:
+            # Old frequencies are completely covered.
+            #     [old frequencies]
+            # [    new frequencies    ]
+            from_ = self.offset - offset_data
+            to = from_ + self.bins - 1
+            self.frequencies = self._add(
+                self.frequencies,
+                freq[from_:to],
+            )
+            self.frequencies = freq[:from_] + self.frequencies + freq[to:]
+            self.offset = offset_data
+            self.bins = len(self.frequencies) + 1
+
+    def combine(self, histogram):
+        """
+        Combine this histogram with another UIntHistogram.
+
+        :param histogram: to merge with this
+        """
+        self._aggregate_histograms(
+            offset_data=histogram.offset,
+            bins=histogram.bins,
+            freq=histogram.frequencies,
+        )
+
+    def update(self, data):
+        """
+        Update histogram by adding more data.
+
+        :param data: array(uint)
+            Data to be added to the histogram.
+        """
+        assert data.min() >= 0, "Negative data is not supported."
+
+        if self.frequencies is None:
+            self.offset, self.bins, self.frequencies = self._get_hist(data)
+        else:
+            offset_data, bins, freq = self._get_hist(data)
+            self._aggregate_histograms(offset_data=offset_data, bins=bins, freq=freq)
+
+    def plot(self, width=1):
+        """
+        Plot histogram.
+        """
+        if width > 1:
+            heights = []
+            for i in range(self.offset, self.offset + self.bins - 1, width):
+                heights.append(np.sum(self.frequencies[i : i + width]))
+        else:
+            heights = self.frequencies
+
+        plt.bar(
+            np.arange(self.offset, self.offset + self.bins - 1, width),
+            heights,
+            width=width,
+        )
+        plt.show()
+
+    def mean(self):
+        """
+        Get histogram mean.
+        :return: float
+        """
+        return np.sum(
+            np.arange(self.offset, self.offset + self.bins - 1) * self.frequencies
+        ) / np.sum(self.frequencies)
+
+    def std(self):
+        """
+        Get histogram standard deviation.
+        :return: float
+        """
+        return np.sqrt(
+            np.sum(
+                (np.arange(self.offset, self.offset + self.bins - 1) - self.mean()) ** 2
+                * self.frequencies
+            )
+            / np.sum(self.frequencies)
+        )
+
+    def quantile(self, q):
+        """
+        Get quantile `q`.
+        :param q: uint
+        :return: quantile
+        """
+        assert q >= 0 and q <= 1
+        return self.offset + np.argmax(
+            np.cumsum(self.frequencies) / np.sum(self.frequencies) >= q
+        )
+
+    def min(self):
+        """
+        Get minimum.
+        :return: uint
+        """
+        return self.offset
+
+    def max(self):
+        """
+        Get maximum.
+        :return: uint
+        """
+        return self.offset + self.bins - 2
+
+    def save(self, path):
+        np.savez(path, frequencies=self.frequencies, offset=self.offset, bins=self.bins)
+
+    @staticmethod
+    def load(path):
+        storage = np.load(path)
+        hist = UIntHistogram()
+        hist.frequencies = storage["frequencies"].tolist()
+        hist.offset = storage["offset"]
+        hist.bins = storage["bins"]
+        return hist
---
+++ tests/test_UIntHistogram.py
@@ -0,0 +1,311 @@
+import tempfile
+import unittest
+from os.path import join
+
+import numpy as np
+from numpy.testing import assert_almost_equal, assert_array_equal, assert_equal
+
+from faim_hcs.UIntHistogram import UIntHistogram
+
+
+class TestUIntHistogram(unittest.TestCase):
+    def test_bins(self):
+        data = np.array([4, 5, 6])
+        hist = UIntHistogram(data)
+        assert hist.bins == len(hist.frequencies) + 1
+
+    def test_update_same_length(self):
+        # Old and new frequencies cover the same range:
+        # [old frequencies]
+        # [new frequencies]
+        data = np.array([4, 5, 6])
+        hist = UIntHistogram(data)
+        hist.update(data)
+
+        assert_array_equal(hist.frequencies, np.array([2, 2, 2]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 4)
+
+    def test_update_lower_overlap(self):
+        # New frequencies have additional lower ones.
+        #     [old frequencies]
+        # [new frequencies]
+        data = np.array([4, 5, 6])
+        update_data = np.array([2, 3, 4])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 1, 2, 1, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 2)
+
+        update_data = np.array([2, 3, 4, 5, 6])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 1, 2, 2, 2]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 2)
+
+    def test_update_lower_concat(self):
+        # New frequencies only have additional lower ones.
+        #                  [old frequencies]
+        # [new frequencies]
+        data = np.array([4, 5, 6])
+        update_data = np.array([1, 2, 3])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 1, 1, 1, 1, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 1)
+
+    def test_update_lower_gap(self):
+        # New frequencies only have additional lower ones.
+        #                       [old frequencies]
+        # [new frequencies]
+        data = np.array([4, 5, 6])
+        update_data = np.array([1, 2])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 1, 0, 1, 1, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 1)
+
+    def test_update_upper_overlap(self):
+        # New frequencies have additional upper ones.
+        #     [old frequencies]
+        #            [new frequencies]
+        data = np.array([4, 5, 6])
+        update_data = np.array([5, 6, 7])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 2, 2, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 4)
+
+        update_data = np.array([4, 5, 6, 7])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([2, 2, 2, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 4)
+
+    def test_update_upper_concat(self):
+        # New frequencies have only additional upper ones.
+        #     [old frequencies]
+        #                      [new frequencies]
+        data = np.array([4, 5, 6])
+        update_data = np.array([7, 8])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 1, 1, 1, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 4)
+
+    def test_update_upper_gap(self):
+        # New frequencies have only additional upper ones.
+        #     [old frequencies]
+        #                           [new frequencies]
+        data = np.array([4, 5, 6])
+        update_data = np.array([8, 9])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 1, 1, 0, 1, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 4)
+
+    def test_update_covered(self):
+        # New frequencies are completely covered.
+        # [          old frequencies          ]
+        #           [new frequencies]
+        data = np.array([4, 5, 6])
+        update_data = np.array([5])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 2, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 4)
+
+        data = np.array([4, 5, 6, 7])
+        update_data = np.array([5, 6])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 2, 2, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 4)
+
+        data = np.array([4, 5, 6, 7])
+        update_data = np.array([4, 5, 6])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([2, 2, 2, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 4)
+
+        data = np.array([4, 5, 6, 7])
+        update_data = np.array([5, 6, 7])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 2, 2, 2]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 4)
+
+    def test_old_frequencies_covered(self):
+        # Old frequencies are completely covered.
+        #     [old frequencies]
+        # [    new frequencies    ]
+        data = np.array([5])
+        update_data = np.array([4, 5, 6])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 2, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 4)
+
+        data = np.array([5])
+        update_data = np.array([3, 5, 6])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 0, 2, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 3)
+
+        data = np.array([5])
+        update_data = np.array([4, 5, 7])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 2, 0, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 4)
+
+        data = np.array([5])
+        update_data = np.array([3, 5, 7])
+        hist = UIntHistogram(data)
+        hist.update(update_data)
+
+        assert_array_equal(hist.frequencies, np.array([1, 0, 2, 0, 1]))
+        assert_equal(hist.bins, len(hist.frequencies) + 1)
+        assert_equal(hist.offset, 3)
+
+    def test_mean(self):
+        data = np.random.randint(0, 255, (100, 100))
+        hist = UIntHistogram(data)
+        assert_almost_equal(hist.mean(), np.mean(data), decimal=10)
+
+        update_data = np.random.randint(9, 127, (100, 100))
+        hist.update(update_data)
+        assert_almost_equal(
+            hist.mean(),
+            np.mean(np.concatenate([data.ravel(), update_data.ravel()])),
+            decimal=10,
+        )
+
+    def test_std(self):
+        data = np.random.randint(0, 255, (100, 100))
+        hist = UIntHistogram(data)
+        assert_almost_equal(hist.std(), np.std(data), decimal=10)
+
+        update_data = np.random.randint(9, 127, (100, 100))
+        hist.update(update_data)
+        assert_almost_equal(
+            hist.std(),
+            np.std(np.concatenate([data.ravel(), update_data.ravel()])),
+            decimal=10,
+        )
+
+    def test_quantile(self):
+        data = np.random.randint(50, 150, (100, 100))
+
+        hist = UIntHistogram()
+        hist.update(data)
+        assert_equal(hist.quantile(0.0), np.quantile(data, 0.0))
+        assert_equal(hist.quantile(0.25), np.quantile(data, 0.25))
+        assert_equal(hist.quantile(0.5), np.quantile(data, 0.5))
+        assert_equal(hist.quantile(0.75), np.quantile(data, 0.75))
+        assert_equal(hist.quantile(1.0), np.quantile(data, 1.0))
+
+        update_data = np.random.randint(9, 227, (100, 100))
+
+        hist.update(update_data)
+        assert_equal(
+            hist.quantile(0.0),
+            np.quantile(np.concatenate([data.ravel(), update_data.ravel()]), 0.0),
+        )
+        assert_equal(
+            hist.quantile(0.25),
+            np.quantile(np.concatenate([data.ravel(), update_data.ravel()]), 0.25),
+        )
+        assert_equal(
+            hist.quantile(0.5),
+            np.quantile(np.concatenate([data.ravel(), update_data.ravel()]), 0.5),
+        )
+        assert_equal(
+            hist.quantile(0.75),
+            np.quantile(np.concatenate([data.ravel(), update_data.ravel()]), 0.75),
+        )
+        assert_equal(
+            hist.quantile(1.0),
+            np.quantile(np.concatenate([data.ravel(), update_data.ravel()]), 1.0),
+        )
+
+    def test_min(self):
+        data = np.random.randint(0, 255, (100, 100))
+        hist = UIntHistogram(data)
+        assert_almost_equal(hist.min(), np.min(data), decimal=10)
+
+        update_data = np.random.randint(9, 127, (100, 100))
+        hist.update(update_data)
+        assert_almost_equal(
+            hist.min(),
+            np.min(np.concatenate([data.ravel(), update_data.ravel()])),
+            decimal=10,
+        )
+
+    def test_max(self):
+        data = np.random.randint(0, 255, (100, 100))
+        hist = UIntHistogram(data)
+        assert_almost_equal(hist.max(), np.max(data), decimal=10)
+
+        update_data = np.random.randint(0, 127, (100, 100))
+        hist1 = UIntHistogram(update_data)
+        hist.combine(hist1)
+        assert_almost_equal(
+            hist.max(),
+            np.max(np.concatenate([data.ravel(), update_data.ravel()])),
+            decimal=10,
+        )
+
+    def test_save_load(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            data = np.array([4, 5, 6])
+            hist = UIntHistogram(data)
+
+            hist.save(join(temp_dir, "hist.npz"))
+
+            hist_ = UIntHistogram.load(join(temp_dir, "hist.npz"))
+
+            assert_equal(hist_.bins, hist.bins)
+            assert_equal(hist_.offset, hist.offset)
+            assert_array_equal(hist_.frequencies, hist.frequencies)
+
+            assert hist != hist_
+            assert isinstance(hist.frequencies, list)
+            assert isinstance(hist.bins, int)
+            assert isinstance(hist.offset, int)
+
+
+if __name__ == "__main__":
+    unittest.main()
