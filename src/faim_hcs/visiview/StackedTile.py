9adbdcf41edb97fa834d9d408d37eaeaacc0a9b5
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2024-01-10 17:28:49 +0100

    Use tifffile.memmap.

--- src/faim_hcs/visiview/StackedTile.py
+++ src/faim_hcs/visiview/StackedTile.py
@@ -1,13 +1,12 @@
+import tifffile
 from numpy._typing import NDArray
-from tifffile import TiffFile
 
 from faim_hcs.stitching import Tile
 
 
 class StackedTile(Tile):
     def load_data(self) -> NDArray:
-        with TiffFile(self.path) as tif:
-            data = tif.asarray()[self.position.z]
+        data = tifffile.memmap(self.path, mode="r")[self.position.z]
 
         data = self._apply_background_correction(data)
         data = self._apply_illumination_correction(data)
