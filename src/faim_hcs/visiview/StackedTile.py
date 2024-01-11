906e5e71233caa7df5047ffca4f8606c30b029ed
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2024-01-11 09:58:16 +0100

    Make StackedTile return the whole 3D stack.

--- src/faim_hcs/visiview/StackedTile.py
+++ src/faim_hcs/visiview/StackedTile.py
@@ -6,7 +6,7 @@ from faim_hcs.stitching import Tile
 
 class StackedTile(Tile):
     def load_data(self) -> NDArray:
-        data = tifffile.memmap(self.path, mode="r")[self.position.z]
+        data = tifffile.memmap(self.path, mode="r")
 
         data = self._apply_background_correction(data)
         data = self._apply_illumination_correction(data)
