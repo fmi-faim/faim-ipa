4404dc22deb69e6b91a17a750f835d44571ebb31
Flurin Sturzenegger <st.flurin@gmail.com>
2024-01-24 18:39:01 +0100

    added parsing for ImageXpress at ZMB

---
+++ src/faim_hcs/hcs/imagexpressZMB/__init__.py
@@ -0,0 +1,5 @@
+from .ImageXpressPlateAcquisition import ImageXpressPlateAcquisition  # noqa: F401
+from .ImageXpressWellAcquisition import ImageXpressWellAcquisition  # noqa: F401
+from .SinglePlaneAcquisition import SinglePlaneAcquisition  # noqa: F401
+from .StackAcquisition import StackAcquisition  # noqa: F401
+from .MixedAcquisition import MixedAcquisition  # noqa: F401
