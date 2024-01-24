986cf34c85c5c671564750bf09ad699aae39d58a
Flurin Sturzenegger <st.flurin@gmail.com>
2024-01-24 18:39:01 +0100

    added parsing for ImageXpress at ZMB

---
+++ examples/ZMBImageXpressStackAcquisitionExample.py
@@ -0,0 +1,46 @@
+import shutil
+from pathlib import Path
+
+from faim_hcs.hcs.acquisition import TileAlignmentOptions
+from faim_hcs.hcs.converter import ConvertToNGFFPlate, NGFFPlate
+from faim_hcs.hcs.imagexpressZMB import StackAcquisition
+from faim_hcs.hcs.plate import PlateLayout
+from faim_hcs.stitching import stitching_utils
+
+
+def main():
+    # Remove existing zarr.
+    shutil.rmtree("md-stack.zarr", ignore_errors=True)
+
+    # Parse MD plate acquisition.
+    plate = StackAcquisition(
+        acquisition_dir=Path(r'/mnt/zmb/zmbstaff/9713/Raw_Data/240117_test-dataset/fsturzenegger-test-dataset_Plate_2653'),
+        alignment=TileAlignmentOptions.GRID,
+    )
+
+    # Create converter.
+    converter = ConvertToNGFFPlate(
+        ngff_plate=NGFFPlate(
+            root_dir=".",
+            name="md-stack",
+            layout=PlateLayout.I96,
+            order_name="order",
+            barcode="barcode",
+        ),
+        yx_binning=2,
+        stitching_yx_chunk_size_factor=2,
+        warp_func=stitching_utils.translate_tiles_2d,
+        fuse_func=stitching_utils.fuse_mean,
+    )
+
+    # Run conversion.
+    converter.run(
+        plate_acquisition=plate,
+        well_sub_group="0",
+        chunks=(1, 512, 512),
+        max_layer=2,
+    )
+
+
+if __name__ == "__main__":
+    main()
